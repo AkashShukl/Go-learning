c9cf5f75b1367adb6e6852e9e4b977cd916707b0
Akash Shukla <shuklaa884@gmail.com>
2022-09-19 15:48:49 +0530

    etl solution

--- golang/etl/etl.go
+++ golang/etl/etl.go
@@ -1,1 +1,16 @@
 package etl
+
+import (
+	"strings"
+)
+
+func Transform(input map[int][]string) map[string]int {
+	result := make(map[string]int)
+
+	for k, v := range input {
+		for _, value := range v {
+			result[strings.ToLower(value)] = k
+		}
+	}
+	return result
+}
