45e43178289125876bb728f26981cea6b3806414
Akash Shukla <shuklaa884@gmail.com>
2022-09-24 13:34:51 +0530

    errer handeling exercise

--- golang/error-handling/error_handling.go
+++ golang/error-handling/error_handling.go
@@ -10,17 +10,13 @@ func aux(resource Resource, input string) (err error, ok1 bool) {
 		if r := recover(); r != nil {
 			nr, ok := r.(FrobError)
 			if ok {
-				// fmt.Println("reaching", nr, nr.defrobTag)
 				resource.Defrob(nr.defrobTag)
 				ok1 = true
 				err = nr
 			}
-			_, true := r.(error)
-			if true {
-				// resource.Close()
-				err = r.(error)
-				ok1 = true
-			}
+			newErrr, true := r.(error)
+			err = newErrr
+			ok1 = true
 		}
 	}()
 	resource.Frob(input)
@@ -31,23 +27,21 @@ func Use(o ResourceOpener, input string) error {
 	resource, err := o()
 	if err != nil {
 		var transientErr TransientError
-		// fmt.Printf("%T", err)
 		if errors.As(err, &transientErr) {
 			for err != nil && errors.As(err, &transientErr) {
 				resource, err = o()
 			}
-		} else if err != nil {
+		}
+		if err != nil {
 			return err
 		}
-	} else {
-		defer resource.Close()
 	}
+	fmt.Println(resource)
+	defer resource.Close()
 
 	err11, ok11 := aux(resource, input)
-	// fmt.Println("=>>>", err11, ok11)
 	if ok11 {
 		err = err11
 	}
-	fmt.Println("=>>>", err)
 	return err
 }
